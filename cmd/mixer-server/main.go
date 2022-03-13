0fefebd7581b1b39b24be863f6c0a9e661da35ba
Nic Johnson <nicjohnson145@hotmail.com>
2022-03-12 18:53:06 -0600

    feat: make port and db path configurable

--- cmd/mixer-server/main.go
+++ cmd/mixer-server/main.go
@@ -3,20 +3,28 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"github.com/nicjohnson145/mixer-service/pkg/auth"
+	"github.com/nicjohnson145/mixer-service/pkg/common"
+	"github.com/nicjohnson145/mixer-service/pkg/drink"
 	"github.com/nicjohnson145/mixer-service/pkg/db"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
+
 func main() {
 	r := mux.NewRouter()
-	db := db.NewDBOrDie("foo-bar.db")
+	db := db.NewDBOrDie(common.DefaultedEnvVar("DB_PATH", "mixer.db"))
 
 	if err := auth.Init(r, db); err != nil {
 		log.Fatal(err)
 	}
+	
+	if err := drink.Init(r, db); err != nil {
+		log.Fatal(err)
+	}
 
-	port := "30000"
+	port := common.DefaultedEnvVar("PORT", "30000")
+
 	log.Info("Listening on port ", port)
-	http.ListenAndServe(":"+port, r)
+	http.ListenAndServe(":" + port, r)
 }
